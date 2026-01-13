e117092f3bc832374ca118b33a9700a4651ff782
harrison townend <harrisontownend@hotmail.com>
2026-01-13 13:29:42 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-
 from .forms import OrderForm
-
-# Create your views here.
-
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -16,6 +12,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Sp6zII10BMycAneUF1SzLC5123VXXviT4RGzsg2sDdlwpIuaGGOVY9nivnH7edlSHxYo078MvSSJxtCFQUuWOyp00V50a910Y',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
+
