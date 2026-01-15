3c8aeba86a5fd35e9583ba4a8878d620da96d9fd
harrison townend <harrisontownend@hotmail.com>
2026-01-15 17:26:37 +0000

    Added profiles app

---
+++ profiles/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def profile(request):
+    """
+    Display the usr's profile
+    """
+
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
