379ac3e2a2ef31b7537300b9fb947a3ec9c2a8b7
harrison townend <harrisontownend@hotmail.com>
2026-01-19 13:03:28 +0000

    Securing the views with decorators

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -7,6 +8,7 @@ from checkout.models import Order
 
 # Create your views here.
 
+@login_required
 def profile(request):
     """
     Display the usr's profile
@@ -34,6 +36,7 @@ def profile(request):
     return render(request, template, context)
 
 
+@login_required
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
